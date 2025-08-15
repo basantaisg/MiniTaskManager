26fae6693e3b365cd0332630ee9abf223a19515e
Basanta <basantaparajuli.11@gmail.com>
2025-08-15 22:28:41 +0545

    task service!

--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task, TaskStatus } from './entities/task.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectRepository(Task) private taskRepo: Repository<Task>) {}
+
+  createTask(title: string, description: string, user: User) {
+    const task = this.taskRepo.create({ title, description, user });
+    return this.taskRepo.save(task);
+  }
+
+  async getTasks(user: User, page = 1, limit = 10, status?: TaskStatus) {
+    const skip = (page - 1) * limit;
+
+    const query = this.taskRepo
+      .createQueryBuilder('task')
+      .where('task.userId = :userId', { userId: user.id })
+      .take(limit)
+      .skip(skip);
+
+    if (status) query.andWhere('task.status= :statu', { status });
+
+    return query.getMany();
+  }
+
+  async updateTask(id: number, data: Partial<Task>, user: User) {
+    const task = await this.taskRepo.findOne({
+      where: { id, user: { id: user.id } },
+    });
+    if (!task) throw new Error('Task not found!');
+    Object.assign(task, data);
+    return this.taskRepo.save(task);
+  }
+
+  async deleteTask(id: number, user: User) {
+    const task = await this.taskRepo.findOne({
+      where: { id, user: { id: user.id } },
+    });
+    if (!task) throw new Error('task not found!');
+    return this.taskRepo.remove(task);
+  }
+}
