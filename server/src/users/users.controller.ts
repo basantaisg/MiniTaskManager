3c8401c86fbb09bd32c80fcc926cd6cc2013edf1
Basanta <basantaparajuli.11@gmail.com>
2025-08-15 22:02:27 +0545

    gg

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get()
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findById(id);
+  }
+
+  @Get()
+  findByEmail(@Param('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserRole } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import bcrypt from 'node_modules/bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private usersRepo: Repository<User>) {}
+
+  async findAll() {
+    return this.usersRepo.find();
+  }
+
+  async createUser(
+    name: string,
+    email: string,
+    password: string,
+    role: UserRole,
+  ) {
+    const hash = await bcrypt.hash(password, 10);
+    const user = this.usersRepo.create({ name, email, password: hash, role });
+
+    return this.usersRepo.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepo.findOne({ where: { email } });
+  }
+
+  async findById(id: number) {
+    return this.usersRepo.findOne({ where: { id } });
+  }
+}
