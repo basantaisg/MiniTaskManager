8329ed1f89d85bdc10ac0243d395d5bd3084eb80
Basanta <basantaparajuli.11@gmail.com>
2025-08-16 08:04:56 +0545

    fixes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,29 +1,27 @@
 import { Module } from '@nestjs/common';
-
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { User } from './users/entities/user.entity';
 import { Task } from './tasks/entities/task.entity';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'B2k25ismine@1$',
-      database: 'mini_task',
+      url: process.env.DATABASE_URL, // 👈 Neon full URL from .env
       entities: [User, Task],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // required for Neon SSL
+      },
     }),
     UsersModule,
     TasksModule,
     AuthModule,
-    ConfigModule.forRoot({}),
   ],
 })
 export class AppModule {}
